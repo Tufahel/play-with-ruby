2e250b64547de3a7658992a697dc705d5289af96
Tufahel <tufahel96@gmail.com>
2022-05-09 17:05:57 +0600

    string methods

---
+++ strings.rb
@@ -0,0 +1,11 @@
+phrase = "    tufahel ahmed is a \"student\"     "
+
+puts phrase.strip()
+puts phrase.strip().upcase()
+puts phrase.downcase()
+puts phrase.length()
+puts phrase.include? "student"
+puts phrase[6]
+puts phrase[4,7]
+puts phrase.index("med")
+puts "HELLO".downcase()
