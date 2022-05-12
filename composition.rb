a5630aef95ddf969ccafb6b5003ac331d2ff3887
Tufahel <tufahel96@gmail.com>
2022-05-12 23:52:52 +0600

    compositon

--- composition.rb
+++ composition.rb
@@ -1,22 +1,58 @@
-class Square
-    attr_accessor :value
+module SpaceShipFlight
+    def thrusters
+        puts "Spaceship has standard thrusters"
+    end
 
-    def initialize
-        @value = ''
+    def foils
+        puts "Spaceship has standard foils"
+    end
+
+    def navigation_system
+        puts "Spaceship has standard navigation system"
     end
 end
 
-class Board
-    def initialize
-        @first_row = [Square.new, Square.new, Square.new]
-        @second_row = [Square.new, Square.new, Square.new]
-        @third_row = [Square.new, Square.new, Square.new]
+module SpaceShipWeapons
+    def torpedos
+        puts "Spaceship has proton torpedos"
     end
 
-    def show
-        puts @first_row[0..2]
+    def laser_cannons
+        puts "Spaceship has laser cannons"
     end
 end
 
-board = Board.new
-board.show
+class X_Wing
+    include SpaceShipFlight
+    include SpaceShipWeapons
+
+    def foils
+        puts "X-Wing has standard foils"
+    end
+
+    def laser_cannons
+        puts "X-Wing has laser cannons"
+    end
+end
+
+class TIE_Fighter
+    include SpaceShipFlight
+    include SpaceShipWeapons
+
+    def foils
+        puts "TIE Fighter has standard foils"
+    end
+
+    def laser_cannons
+        puts "TIE Fighter has laser cannons"
+    end
+end
+
+red_five = X_Wing.new
+mauler_mithel = TIE_Fighter.new
+
+red_five.laser_cannons
+red_five.foils
+
+mauler_mithel.laser_cannons
+mauler_mithel.foils
