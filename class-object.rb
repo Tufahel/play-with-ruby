e647f24e4b3e38d12e8931d6127129fd638c98cc
Tufahel <tufahel96@gmail.com>
2022-05-12 15:47:27 +0600

    class object

--- class-object.rb
+++ class-object.rb
@@ -1,19 +1,18 @@
 class Animal
+    attr_accessor :name, :number_of_legs
+    attr_reader :id
     def initialize(number_of_legs, name = "Unknown")
         @id = Random.rand(1..1000)
         @name = name
         @number_of_legs = number_of_legs
     end
-    def to_s
-        @name
-    end
     def speak
-        "Bla bla bla"
+        puts ("bla bla bla #{name} #{number_of_legs}")
     end
 end
 
 animal_1 = Animal.new(4, "Rex")
 animal_2 = Animal.new(8)
 
-animal_1
+animal_1.speak
 animal_2
