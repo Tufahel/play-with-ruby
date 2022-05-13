76e6345c1ee28770c5044b262e7418a3f38580a5
Tufahel <tufahel96@gmail.com>
2022-05-14 00:43:19 +0600

    four principal and composition

---
+++ oop-principle-example/animal.rb
@@ -0,0 +1,26 @@
+require "./remover.rb"
+
+class Animal
+    def initialize(type, number_of_legs, name = 'Unknown')
+        @id = Random.rand(1..1000)
+        @name = name
+        @number_of_legs = number_of_legs
+        @type = type
+        @liked_food = NoFood.new()
+    end
+
+    attr_accessor :name
+
+    def speak
+        "grrrr"
+    end
+
+    def remove_leg
+        remover = Remover.new()
+        @number_of_legs = remover.decrease(@number_of_legs)
+    end
+
+    def likes_food?(food)
+        @liked_food.is_liked?(food)
+    end
+end
